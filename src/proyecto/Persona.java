4f6c8ba22c2f65c54958612c4a04a85b640168be
Javier Pereyra <javier.pereyra183@gmail.com>
2020-08-29 16:23:53 -0700

    Codigo Indaburo classes java

--- src/proyecto/Persona.java
+++ src/proyecto/Persona.java
@@ -2,4 +2,41 @@ package proyecto;
 
 public class Persona {
 
+	private int dni;
+	private String nombre;
+	private String apellido;
+	
+	//gets y sets
+	public int getDni() {
+		return dni;
+	}
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	// constructores
+	
+	public Persona() {
+		dni=11111111;
+		nombre = "Sin Nombre";
+		apellido = "Sin Apellido";
+	}
+	
+	public Persona(String nombre, String apellido, int dni) {
+		setNombre(nombre);
+		setApellido(apellido);
+		setDni(dni);
+	}
 }
