cfe39f9f859de7e5ba42f1c46d49308cd20a0889
Javier Pereyra <javier.pereyra183@gmail.com>
2020-08-31 21:48:01 -0700

    leo archivo, genero unos arrays

--- src/proyecto/Principal.java
+++ src/proyecto/Principal.java
@@ -2,27 +2,36 @@ package proyecto;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		
+
+		// Lista de Personas
+		ArrayList<String> elementos = new ArrayList<String>();
+
 		// Leer el archivo llamado Personas Empresa.txt
-		
-		// defino ruta
-		String ruta = "PersonasEmpresas.txt";
+		String ruta = "PersonasEmpresa.txt ";
 		FileReader entrada;
 		try {
 			entrada = new FileReader(ruta);
 			BufferedReader miBuffer = new BufferedReader(entrada);
+			ArrayList<String> auxiliar = new ArrayList<String>();
 			
-		   String linea = "";
-			while (linea != null) {
-				System.out.println(linea);
-				linea = miBuffer.readLine();
-			}
-			miBuffer.close();
-			entrada.close();
+			
+			   String linea = "";
+				while (linea != null) {
+
+					String[] cadalinea = linea.split("-");
+					for (String cl : cadalinea) {
+						elementos.add(cl);
+					}
+					
+				}
+				
+				miBuffer.close();
+				entrada.close();
 
 		} catch (IOException e) {
 			System.out.println("No se encontro el archivo");
