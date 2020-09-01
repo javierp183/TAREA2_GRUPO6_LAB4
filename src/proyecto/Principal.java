a89ff36a2fea1a97763bea26e54b3489a691eca4
Javier Pereyra <javier.pereyra183@gmail.com>
2020-08-31 19:53:33 -0700

    Leo texto de proyecto

--- src/proyecto/Principal.java
+++ src/proyecto/Principal.java
@@ -1,45 +1,36 @@
 package proyecto;
-
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class Principal {
 
-	private static String route = "/PersonasEmpresa.txt";
-
 	public static void main(String[] args) {
 		
-		File archive = new File(route);
-		if(archive.exists()){
+		// Leer el archivo llamado Personas Empresa.txt
+		
+		// defino ruta
+		String ruta = "PersonasEmpresas.txt";
+		FileReader entrada;
+		try {
+			entrada = new FileReader(ruta);
+			BufferedReader miBuffer = new BufferedReader(entrada);
+			
+		   String linea = "";
+			while (linea != null) {
+				System.out.println(linea);
+				linea = miBuffer.readLine();
+			}
+			miBuffer.close();
+			entrada.close();
 
+		} catch (IOException e) {
+			System.out.println("No se encontro el archivo");
 		}
 		
+		
+		
+		
 	}
-
-	public void lee_lineas() {
-        FileReader entrada;
-        try {
-            entrada = new FileReader(route);
-            BufferedReader miBuffer = new BufferedReader(entrada);
-            
-            ArrayList<Persona> PersonaList = new ArrayList<Persona>();
-            
-           String linea = "";
-            while (linea != null) {
-                System.out.println(linea);
-               // PersonaList.add(); 
-                
-                linea = miBuffer.readLine();
-            }
-            miBuffer.close();
-            entrada.close();
-
-        } catch (IOException e) {
-            System.out.println("No se encontro el archivo");
-        }
-    }
 
 }
